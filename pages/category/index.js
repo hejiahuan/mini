4f15acbbb0a2f1911185a04c5ad58794a171989a
hejiahuan <609296480@qq.com>
2020-03-23 15:30:13 +0800

    加了缓存主要是靠wx.setStorageSync

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,5 +1,5 @@
 
-import {getCategoryList } from "../../network/category"
+import { getCategoryList } from "../../network/category"
 
 Page({
 
@@ -8,34 +8,91 @@ Page({
    */
   data: {
     //左侧的菜单数据
-    leftMenuList:[],
+    leftMenuList: [],
     //右侧的商品数据
-    rightContent:[],
+    rightContent: [],
     //分类的总数据
-    categoryList:[],
+    categoryList: [],
     //当前页码
-    currentIndex:0
+    currentIndex: 0,
+    //缓存用的
+    Cates: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    getCategoryList().then(res=>{
-     this.categoryList=res.data.message;
-     //构造左侧的大菜单数据这里用了js的map遍历数组
-     let leftMenuList=this.categoryList.map(v=>v.cat_name);
-     //构造右侧的商品数据
-     let rightContent=this.categoryList[0].children;
-     this.setData({
-      leftMenuList,
-      rightContent
-     })
+    // 格式{time:data,data:{}}
+    // 1先判断是否本地是否有旧数据
+    // 2没有旧数据发送请求
+    // 3有旧数据同时没有过期用旧数据
+    // 0以前web本地存储和小程序的存储区别
+    // web:localStorage.setItem("key","value")
 
-    })
+    // 1获取本地存储的数据
+    const Cates = wx.getStorageSync("cates");
+    if (!Cates) {
+      //不存在发送请求获取数据
+      getCategoryList().then(res => {
+        this.categoryList = res.data.message;
+        //构造左侧的大菜单数据这里用了js的map遍历数组
+        let leftMenuList = this.categoryList.map(v => v.cat_name);
+
+        //把接口的数据存入到本地存储中
+        wx.setStorageSync("cates", { time: Date.now(), data: this.categoryList });
+
+
+        //构造右侧的商品数据
+        let rightContent = this.categoryList[0].children;
+        this.setData({
+          leftMenuList,
+          rightContent
+        })
+
+      })
+    } else {
+      //有旧数据，是否过期定义一个过期时间 测试10s
+      // 1000ms
+      if (Date.now - Cates.time > 1000 * 10) {
+        //重新发送请求
+        //不存在发送请求获取数据
+        getCategoryList().then(res => {
+          this.categoryList = res.data.message;
+          //构造左侧的大菜单数据这里用了js的map遍历数组
+          let leftMenuList = this.categoryList.map(v => v.cat_name);
+
+          //把接口的数据存入到本地存储中
+          wx.setStorageSync("cates", { time: Date.now(), data: this.categoryList });
+
+
+          //构造右侧的商品数据
+          let rightContent = this.categoryList[0].children;
+          this.setData({
+            leftMenuList,
+            rightContent
+          })
+
+        })
+      } else {
+        this.Cates = Cates.data;
+           //构造左侧的大菜单数据这里用了js的map遍历数组
+           let leftMenuList = this.Cates.map(v => v.cat_name);
+           //构造右侧的商品数据
+           let rightContent = this.Cates[0].children;
+           this.setData({
+             leftMenuList,
+             rightContent
+           })
+      }
+    }
+
+
+
   },
 
- 
+
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
@@ -86,16 +143,16 @@ Page({
 
   },
   // 左侧点击事件
-  bindClick(e){
-    let currentIndex=e.currentTarget.dataset.index;
+  bindClick(e) {
+    let currentIndex = e.currentTarget.dataset.index;
     //构造右侧的商品数据
-    let rightContent=this.categoryList[currentIndex].children;
-    
+    let rightContent = this.Cates[currentIndex].children;
+
     this.setData({
-    currentIndex,
-     rightContent
+      currentIndex,
+      rightContent
     })
-    
+
   },
 
 })
